09c836a9c0746bb55508cc7d53a2ff932fc4c64c
201702083 <201702083@o.cnu.ac.kr>
2021-07-06 10:26:03 +0900

    Load frag to Main frag btn

--- app/src/main/java/com/example/zzik_kong/fragment/LoadFragment.kt
+++ app/src/main/java/com/example/zzik_kong/fragment/LoadFragment.kt
@@ -1,4 +1,39 @@
 package com.example.zzik_kong.fragment
 
-class LoadFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.zzik_kong.R
+import com.example.zzik_kong.databinding.FragmentLoadBinding
+
+class LoadFragment : Fragment(){
+
+    private var mBinding : FragmentLoadBinding? = null
+    private val binding get() = mBinding!!
+    lateinit var navController : NavController
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentLoadBinding.inflate(inflater,container,false)
+        mBinding = binding
+        return mBinding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        binding.kongBtn.setOnClickListener{
+            navController.navigate(R.id.action_loadFragment_to_loginFragment)
+        }
+    }
+    override fun onDestroyView() {
+        mBinding=null
+        super.onDestroyView()
+    }
 }
